c8b3c7c688a52b8cb960c67aa3ddbabcadfce166
G0rgutzz <grzegorz.pyclik1@gmail.com>
2023-07-05 13:27:59 +0200

    updates regarding card values, comments about function and variables goals

--- day 11.py
+++ day 11.py
@@ -2,17 +2,26 @@ import random
 
 
 def blackjack():
-    cards_values = [11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    dealer_value = 0
-    player_value = 0
-    cards_amount = 2
-    bet = 0
-    turn = 1
-    play = True
+    cards_values = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]  # cards values,
+    # to be tied with their names later
+    dealer_value = 0  # value of the dealer's cards
+    player_value = 0  # value of the player's cards
+    cards_amount = 2  # to be used later
+    bet = 0  # same as above
+    turn = 1  # same as above
+    play = True  # states if the player wants to play more, initially True
     while play is True:
-        dealer_value += random.choice(cards_values)
-        player_value += random.choice(cards_values)
-        print(f"Dealer value is {dealer_value}\nYour value is {player_value}.")
+        if turn == 1:
+            for i in range(0, cards_amount):
+                dealer_value += random.choice(cards_values)
+                player_value += random.choice(cards_values)
+            print(f"Dealer value is {dealer_value}\nYour value is {player_value}.")
+            if player_value == 21:
+                print(f"Congratulations, you've hit a blackjack.")
+        else:
+            dealer_value += random.choice(cards_values)
+            player_value += random.choice(cards_values)
+            print(f"Dealer value is {dealer_value}\nYour value is {player_value}.")
         if dealer_value >= 21 or player_value >= 21:
             if player_value > 21 and dealer_value > 21:
                 print(f"You draw with dealer with {dealer_value} points after {turn} turns.")
