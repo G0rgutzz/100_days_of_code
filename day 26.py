b58c78fa1cb34f9378e96cdc78858694b5f53612
G0rgutzz <grzegorz.pyclik1@gmail.com>
2023-08-19 15:15:42 +0200

    final code for the 26th day of the course, added nato alphabet project

--- day 26.py
+++ day 26.py
@@ -1,4 +1,7 @@
 import random
+
+import pandas as pd
+"""
 # few exercises to learn list comprehensions
 numbers = [1, 2, 3]
 new_numbers = [n+1 for n in numbers]
@@ -14,3 +17,37 @@ students_scores = {name:random.randint(1, 100) for name  in names}
 print(students_scores)
 passed_students = {name:value for (name, value) in students_scores.items() if value>70}
 print(passed_students)
+
+# coding exercise
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+words = sentence.split()
+result = {word:len(word) for word in words}
+print(result)
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 78, 90]
+}
+
+student_df = pandas.DataFrame(student_dict)
+# print(student_df)
+# looping a data frame
+for (key, value) in student_df.items():
+    print(value)
+
+# looping through data frame rows
+for (index, row) in student_df.iterrows():
+    print(row)
+"""
+# NATO alphabet project
+alphabet = pd.read_csv('nato_phonetic_alphabet.csv')
+letter_dict = {row.letter:row.code for (index, row) in alphabet.iterrows()}
+# print(letter_dict)
+#letter_dict_test1 = alphabet.to_dict()  # not the same format as above
+# print(letter_dict_test1)
+word = input("Input a word: ").upper()
+word_list = []
+for x in word:
+    print(letter_dict[x])
+    word_list.append(letter_dict[x])
+
