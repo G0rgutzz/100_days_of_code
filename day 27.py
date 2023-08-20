685656b4f62682940450cfc8b6c6144398c7d4b5
G0rgutzz <grzegorz.pyclik1@gmail.com>
2023-08-20 22:21:21 +0200

    code from the 27th day of the course, couple tries to mess around with GUI and tkinter. Added whole miles to km converter with 2 decimal points precision

--- day 27.py
+++ day 27.py
@@ -1,13 +1,52 @@
 import tkinter
 
 window = tkinter.Tk()
-window.title("HEHEHE eltit is backwards")  # title of the window
-window.minsize(width=600, height=600)  # minimal size of the window
+window.title("retarded units to kilometers converter")  # title of the window
+window.minsize(width=400, height=300)  # minimal size of the window
+# window.config(padx=20, pady=20)  # padding
+"""
 # label
 my_label = tkinter.Label(text="GURRWAAA", font=('Times New Roman', 20, 'normal'))
 #specyfying how the label is displayed
-my_label.pack(side='left')  # allows the label to be shown
+# my_label.pack()  # packs the widgets starting from the top to bottom
+# my_label['text'] = 'new text'  # changing the text of the label
+# my_label.place(x=0, y=0)  # places the label on the specified coordinates
+my_label.grid(column=0, row=0)  # places the widget according the specified position on the grid
+grid, place, pack is not compatible with each other. Only one can be used
+# buttons
+def click():
+    my_label['text'] = New_input.get()  # gets the input and makes it the label name
 
+
+button = tkinter.Button(text='KLIKNIJ MNIE!', command=click)
+button.grid(column=1, row=1)
+#button nr 2
+button2 = tkinter.Button(text='METALE CIĘŻKIE ZABIJAJĄ!', command=click)
+button2.grid(column=2, row=0)
+# Input
+New_input = tkinter.Entry(width=20)
+New_input.grid(column=3, row=2)
+"""
+my_label1 = tkinter.Label(text="         ", font=('Times New Roman', 12, 'normal'))
+my_label1.grid(column=0, row=0)
+my_label = tkinter.Label(text="Miles to km converter", font=('Times New Roman', 14, 'normal'))
+my_label.grid(column=1, row=0)
+
+def click():
+    kilometers['text'] = f"{round(float(miles.get())*1.609344, 2)}"  # gets the input and makes it the label name
+
+
+button = tkinter.Button(text='Convert', command=click)
+button.grid(column=1, row=1)
+miles_label = tkinter.Label(text=f"Miles: ", font=('Times New Roman', 12, 'normal'))
+miles_label.grid(column=0, row=2)
+miles = tkinter.Entry(width=10)
+miles.grid(column=1, row=2)
+kilometers_label = tkinter.Label(text=f"Kilometers: ", font=('Times New Roman', 12, 'normal'))
+kilometers_label.grid(column=0, row=3)
+kilometers = tkinter.Label(text=f" ", font=('Times New Roman', 12, 'normal'))
+kilometers.grid(column=1, row=3)
 
 
 window.mainloop()
+
